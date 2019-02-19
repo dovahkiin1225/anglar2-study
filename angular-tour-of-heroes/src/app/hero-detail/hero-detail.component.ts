ae2219dfec590b951fb178c11e06d4307edc4e83
pengyx <pengyx@szinfinova.com>
2019-02-19 17:08:37 +0800

    feat:创建了一个独立的、可复用的 HeroDetailComponent 组件

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';     //导入 Input 符号。
+import { Hero } from '../hero';       //导入 Hero 符号
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero: Hero;      //添加一个带有 @Input() 装饰器的 hero 属性。
   constructor() { }
 
   ngOnInit() {
