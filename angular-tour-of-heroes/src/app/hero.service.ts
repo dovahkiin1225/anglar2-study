4c1ed2f43a579544be738a1d1953a8c542a500aa
pengyx <pengyx@szinfinova.com>
2019-02-20 11:41:04 +0800

    feat:数据访问逻辑重构到了 HeroService 类

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -5,6 +5,8 @@ import { HEROES } from './mock-heroes';
 
 import { Observable, of } from 'rxjs';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +15,11 @@ export class HeroService {
   //   return HEROES;
   // };
   getHeroes(): Observable<Hero[]> {   //之前返回一个 Hero[]， 现在它返回的是 Observable<Hero[]>。
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor() { }
+  // constructor() { }
+  // 把 MessageService 注入到了 HeroService 中，而 HeroService 又被注入到了 HeroesComponent 中。
+  constructor(private messageService: MessageService) { }
 }
