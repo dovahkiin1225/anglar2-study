67396913b441d0e4925cded3f2f83dcb593a512a
pengyx <pengyx@szinfinova.com>
2019-02-20 15:30:09 +0800

    feat:添加了路由

---
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+/* import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
+ */
+
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent }      from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
+})
+export class AppRoutingModule {}
+
